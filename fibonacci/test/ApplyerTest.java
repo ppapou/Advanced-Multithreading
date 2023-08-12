328124ba5ff31d3a133be62c23d265c7b6b274e4
ppapou <Pavel Papou>
2023-08-11 22:37:05 -0400

    Recursive task
     - docstring and test has added

--- fibonacci/test/ApplyerTest.java
+++ fibonacci/test/ApplyerTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ApplyerTest {
+import org.junit.Test;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ApplyerTest {
+    ForkJoinPool pool;
+    double array [];
+    @Test
+    double sumOfSquares() {
+        int n = array.length;
+        Applyer a = new Applyer(array, 0, n, null);
+        pool.invoke(a);
+        return a.result;
+    }
 }
